b67b18181aa6b4344dec65d72277ef5edb22ca0f
Mingrui Wu <mwusdv@gmail.com>
2019-01-13 15:45:59 -0800

    lane_find.py is good for detecting line segments.

---
+++ lane_find.py
@@ -0,0 +1,407 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 20:37:10 2019
+
+@author: mingrui
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#importing some useful packages
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import os
+#%matplotlib inline
+
+import math
+
+def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    (assuming your grayscaled image is called 'gray')
+    you should call plt.imshow(gray, cmap='gray')"""
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Or use BGR2GRAY if you read an image with cv2.imread()
+    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    `vertices` should be a numpy array of integer points.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)
+    
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to 
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).  
+    
+    Think about things like separating line segments by their 
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of 
+    the lines and extrapolate to the top and bottom of the lane.
+    
+    This function draws `lines` with `color` and `thickness`.    
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
+    """
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
+    
+    `initial_img` should be the image before any processing.
+    
+    The result image is computed as follows:
+    
+    initial_img * α + img * β + γ
+    NOTE: initial_img and img must be the same shape!
+    """
+    return cv2.addWeighted(initial_img, α, img, β, γ)
+
+
+
+####################################
+# My code starting from here
+###################################
+
+# all the parameters needed in the whole 
+# lane detection pipeline
+class LaneDetectParam:
+    def __init__(self, ir_row_ratio=0.4, ir_upper_col_ratio = 0.05, ir_lower_col_ratio=1, \
+                 kernel_size=5, canny_low_threshold=50, canny_high_threshold=150, \
+                 rho=1, theta=np.pi/180, min_vote=5, min_line_len=5, max_line_gap=2, \
+                 line_color = [255, 0, 0], line_thickness = 10):
+        # for color detection
+        self.white_rgb_threshold = [90, 90, 90]
+        self.yellow_hsv_lb = np.array([15, 100, 100])
+        self.yellow_hsv_ub = np.array([40, 255, 255])
+        
+        # for interest region
+        self.ir_row_ratio = ir_row_ratio
+        self.ir_upper_col_ratio = ir_upper_col_ratio
+        self.ir_lower_col_ratio = ir_lower_col_ratio
+        
+        # for Gaussian smoothing
+        self.kernel_size = kernel_size
+        
+        # for canny edge detection
+        self.canny_low_threshold = canny_low_threshold
+        self.canny_high_threshold = canny_high_threshold
+      
+       
+      
+        # for hough tranform
+        self.rho = rho
+        self.theta = theta
+        self.min_vote = min_vote
+        self.min_line_len = min_line_len
+        self.max_line_gap = max_line_gap
+        
+        # for line drawing
+        self.line_color = line_color
+        self.line_thickness = line_thickness
+        
+# detect white areas
+def detect_white(image, param):
+    mask = (image[:,:,0] > param.white_rgb_threshold[0]) \
+         & (image[:,:,1] > param.white_rgb_threshold[1]) \
+         & (image[:,:,2] > param.white_rgb_threshold[2]) \
+    
+    return mask
+
+# detect yellow areas
+def detect_yellow(image, param):
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    mask = cv2.inRange(hsv, param.yellow_hsv_lb, param.yellow_hsv_ub)
+    
+    return mask > 0
+    
+# mask of interest region based on colors
+def get_color_mask(image, param):
+    mask_white = detect_white(image, param)
+    mask_yellow = detect_yellow(image, param)
+    mask = mask_white | mask_yellow
+    
+    return mask
+
+# vertices of the interest region
+def get_interest_region_vertices(image, param):
+    row, col = image.shape[:2]
+    mid = col//2
+ 
+    # height of the interest region
+    height = int(row * param.ir_row_ratio)
+    
+    # length of the upper and lower horizontal edges
+    upper_edge_length = int(col * param.ir_upper_col_ratio)
+    lower_edge_length = int(col * param.ir_lower_col_ratio)
+    offset = (col - lower_edge_length) // 2
+    
+    # trapezoid
+    vertices = np.array([[(offset, row-1), (col-1-offset, row-1), \
+                          (mid+upper_edge_length//2, row-height), (mid-upper_edge_length//2, row-height)]])
+    return vertices
+
+# mask of interest region based on positions
+def get_region_mask(image, param):
+    vertices = get_interest_region_vertices(image, param)
+    
+    if len(image.shape) > 2:
+        mask = np.zeros_like(image[:, :, 0])
+    else:
+        mask = np.zeros_like(image)
+    mask = cv2.fillPoly(mask, vertices, 1) > 0
+   
+    return mask
+
+def get_mask(image, param):
+    region_mask = get_region_mask(image, param)
+    color_mask = get_color_mask(image, param)   
+
+    mask = region_mask & color_mask
+    return mask
+
+
+# get the two end points of a line for drawing
+def get_end_points(img, line):
+    slope, interception = line
+    row = img.shape[0]
+    col = img.shape[1]
+    y1 = row-1
+    y2 = int(row * 0.6)
+    
+    x1 = int((y1 - interception)/slope)
+    x2 = int((y2 - interception)/slope)
+    
+    return x1, y1, x2, y2
+
+def expand_lines(lines):
+    points = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        points.append([x1, y1])
+        points.append([x2, y2])
+    
+    expanded_lines = []
+    N = len(points)    
+    for m in range(N-1):
+        x1, y1 = points[m]
+     
+        for n in range(m+1, N):
+            x2, y2 = points[n]
+            expanded_lines.append(np.array([[x1, y1, x2, y2]]))
+            
+    return np.array(expanded_lines)
+
+def get_left_right_lines(lines, image):
+    col = image.shape[1]
+    lb = col/3
+    rb = col-lb
+    
+    left_lines = []
+    right_lines = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        if x1 == x2:
+            continue
+        param = np.polyfit((x1, x2), (y1, y2), 1)
+   
+        slope = param[0]
+        if slope < 0 and x1 < rb and x2 < rb:
+            left_lines.append(line)
+        elif slope > 0 and x1 > lb and x2 > lb:
+            right_lines.append(line)
+            
+    return np.array(left_lines), np.array(right_lines)
+                
+def get_average_line(lines, slb, sub):
+    # expand point pairs
+    expanded_lines = expand_lines(lines)
+    
+    # get slopes and interceptions
+    slope_intercepts = []
+    for line in expanded_lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        if x1 == x2:
+            continue
+        param = np.polyfit((x1, x2), (y1, y2), 1)
+      
+        slope = param[0]
+        if slope > sub or slope < slb:
+            continue
+        
+        interception = param[1]
+        
+        slope_intercepts.append(np.array([slope, interception]))
+    slope_intercepts = np.array(slope_intercepts)
+    
+    # robust mean
+    N = len(slope_intercepts)
+    tail = max(2, int(N*0.2))
+    
+    slopes = np.sort(slope_intercepts[:,0])
+    intercepts = np.sort(slope_intercepts[:,1])
+    
+    slope_ub = slopes[-tail]
+    slope_lb = slopes[tail-1]
+    intercept_ub = intercepts[-tail]
+    intercept_lb = intercepts[tail-1]
+    
+    robust_lines = [line for line in slope_intercepts if line[0] > slope_lb and line[0] < slope_ub \
+                    and line[1] > intercept_lb and line[1] < intercept_ub]
+    return np.mean(robust_lines, axis=0)
+
+        
+def draw_average_lines(img, lines, param):
+    left_lines, right_lines = get_left_right_lines(lines, img)
+    
+    left_line = get_average_line(left_lines, -10000, -0.2)
+    right_line = get_average_line(right_lines, 0.2, 10000)
+    
+    xl1, yl1, xl2, yl2 = get_end_points(img, left_line)
+    xr1, yr1, xr2, yr2 = get_end_points(img, right_line)
+    
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    cv2.line(line_img, (xl1, yl1), (xl2, yl2), param.line_color, param.line_thickness)
+    cv2.line(line_img, (xr1, yr1), (xr2, yr2), param.line_color, param.line_thickness)
+   
+    return line_img
+   
+# given the output of a Canny transform, 
+# return an image with hough lines drawn
+def hough_lines(edge_image, param):
+    lines = cv2.HoughLinesP(edge_image, param.rho, param.theta, param.min_vote, np.array([]), param.min_line_len, param.max_line_gap)
+    line_img = np.zeros((edge_image.shape[0], edge_image.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines, param.line_color, param.line_thickness)
+    return line_img
+
+# given the output of a Canny transform, 
+# return an image with full extent lanes
+def hough_full_lines(edge_image, param):
+    lines = cv2.HoughLinesP(edge_image, param.rho, param.theta, param.min_vote, np.array([]), param.min_line_len, param.max_line_gap)
+    line_img = np.zeros((edge_image.shape[0], edge_image.shape[1], 3), dtype=np.uint8)
+    line_img = draw_average_lines(line_img, lines)
+
+    return line_img
+
+
+# lane find pipeline
+# mode 0: find lane line segments
+# mode 1: find full extent of the lanes
+def lane_find(image, param, mode, color=[255, 0, 0], thickness=10):
+    gray_img = grayscale(image)
+    
+    # edge detection
+    blurred_img = gaussian_blur(gray_img, param.kernel_size)
+    edge_image = canny(blurred_img, param.canny_low_threshold, param.canny_high_threshold)
+    
+    # masked edge image
+    mask = get_mask(image, param)
+    masked_edge_image = edge_image & mask
+    
+    # find lane lines
+    if mode == 0:
+        line_image = hough_lines(masked_edge_image, param)
+    else:
+        line_image = hough_full_lines(masked_edge_image, param)
+    
+    # combine with the input image
+    with_line_image = weighted_img(line_image, image)
+    
+    return with_line_image, line_image, edge_image, masked_edge_image
+
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+def save_frames():
+    T1 = 0
+    T2 = 4
+    N = 40
+    clip1 = VideoFileClip("test_videos/challenge.mp4").subclip(T1, T2)
+    for t in range(N):
+        clip1.save_frame('tmpvideos/frame_' + str(t) + '.jpg', t=(T2-T1)*t/N)
+
+
+# test lane find
+def test_lane_find(input_path, output_path, mode=1, show_image=False, debug_image_name=''):
+    # parameter
+    param = LaneDetectParam()
+    
+    # detect lanes in the images in the input path
+    image_list = os.listdir(input_path)
+    for image_name in image_list:
+     
+        # show_image=True means just debug for the given image
+        if show_image and image_name != debug_image_name:
+           continue
+         
+        print(image_name)
+        I = mpimg.imread(os.path.join(input_path, image_name))
+        with_line_image, line_image, edge_image, masked_edge_image = lane_find(I, param, mode)
+        
+        if show_image:
+            plt.imshow(I)
+            plt.show()
+
+            plt.imshow(with_line_image)
+            plt.show()
+            
+            plt.imshow(line_image)
+            plt.show()
+            
+            plt.imshow(masked_edge_image)
+            plt.show()
+        else:
+            mpimg.imsave(os.path.join(output_path, image_name), with_line_image)
+        
+        
+        
+        
+        
+if __name__ == '__main__':
+    #test_lane_find('frames', 'frames_output', mode=0, show_image=True, debug_image_name='frame_0.jpg')
+    test_lane_find('test_images', 'test_images_output', mode=0, show_image=False, debug_image_name='solidWhiteCurve.jpg')
+    #save_frames()
